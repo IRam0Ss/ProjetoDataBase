1e52333757e496ebc8ba83bf5f9b63b9c3c313fa
Iury <iurysodre22@gmail.com>
2026-01-31 15:54:00 -0300

    feat: Implement country code conversion to ISO3 in data processing and add multiple filtering options to the Streamlit application.

--- TratamentoDataFrame.py
+++ TratamentoDataFrame.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pycountry
 
 df = pd.read_csv(
     "https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv"
@@ -40,6 +41,23 @@ df_limpo = df.dropna()
 
 # conversao de tipo, ano tem que ser int64
 df_limpo = df_limpo.assign(work_year=df_limpo["work_year"].astype("int64"))
+
+
+# corecao do pais
+def convert_iso2_to_iso3(iso2):
+    try:
+        return pycountry.countries.get(alpha_2=iso2).alpha_3
+    except:  # noqa: E722
+        return None
+
+
+# converte as siglas para iso3
+df_limpo = df_limpo.assign(
+    employee_residence=convert_iso2_to_iso3(df_limpo["employee_residence"])
+)
+df_limpo = df_limpo.assign(
+    company_location=convert_iso2_to_iso3(df_limpo["company_location"])
+)
 
 # exporta para cvs a database tratada
 df_limpo.to_csv("dataBase_salary.csv", index=False)
