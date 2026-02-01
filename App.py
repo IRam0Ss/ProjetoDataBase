a26d7fcd21b8a7ef31416ea663f0c51c5330c59d
Iury <iurysodre22@gmail.com>
2026-02-01 01:30:04 -0300

    refactor: Sort unique values for all sidebar filter options.

--- App.py
+++ App.py
@@ -20,40 +20,40 @@ df = pd.read_csv(
 st.sidebar.header(":material/filter_list: Filtros")
 
 # filtros
-years_options = df["work_year"].unique().tolist()
+years_options = sorted(df["work_year"].unique())
 years_selected = st.sidebar.multiselect(
     "Anos", years_options, placeholder="Selecione os anos"
 )
 
-experience_level_options = df["experience_level"].unique().tolist()
+experience_level_options = sorted(df["experience_level"].unique())
 experience_level_selected = st.sidebar.multiselect(
     "Nivel de experiencia",
     experience_level_options,
     placeholder="Selecione o nivel de experiencia",
 )
 
-employment_type_options = df["employment_type"].unique().tolist()
+employment_type_options = sorted(df["employment_type"].unique())
 employment_type_selected = st.sidebar.multiselect(
     "Tipo de contrato",
     employment_type_options,
     placeholder="Selecione o tipo de contrato",
 )
 
-job_title_options = df["job_title"].unique().tolist()
+job_title_options = sorted(df["job_title"].unique())
 job_title_selected = st.sidebar.multiselect(
     "Titulo do cargo",
     job_title_options,
     placeholder="Selecione o titulo do cargo",
 )
 
-company_size_options = df["company_size"].unique().tolist()
+company_size_options = sorted(df["company_size"].unique())
 company_size_selected = st.sidebar.multiselect(
     "Tamanho da empresa",
     company_size_options,
     placeholder="Selecione o tamanho da empresa",
 )
 
-company_location_options = df["company_location"].unique().tolist()
+company_location_options = sorted(df["company_location"].dropna().unique())
 company_location_selected = st.sidebar.multiselect(
     "Localizacao da empresa",
     company_location_options,
